6307a51824c67662ed0cf38943896f7a1147c710
parkito <artyom-karnov@yandex.ru>
2019-03-21 23:15:56 +0300

    Reorganize repository

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+
+group = "ru.siksmfp.kotlin.streams"
+version = "0.1.1"
+
+val appArchiveName = "stream-$version"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.20"
+}
+
+application {
+    mainClassName = "samples.HelloWorldKt"
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+val jar by tasks.getting(Jar::class) {
+    archiveName = "$appArchiveName.jar"
+    into("META-INF") {
+    }
+}
+
