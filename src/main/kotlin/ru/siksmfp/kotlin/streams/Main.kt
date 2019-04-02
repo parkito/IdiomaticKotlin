22b89890e8cda170f5284d2433ca265b2f5e9f60
parkito <artyom-karnov@yandex.ru>
2019-04-03 00:07:24 +0300

    Add todos

--- src/main/kotlin/ru/siksmfp/kotlin/streams/Main.kt
+++ src/main/kotlin/ru/siksmfp/kotlin/streams/Main.kt
@@ -5,3 +5,14 @@ import ru.siksmfp.kotlin.streams.interpreter.ParameterInterpreter
 fun main(args: Array<String>) {
     ParameterInterpreter(args).run()
 }
+
+//todo
+//0 tests
+//1 improve performance by
+// not storing input streams
+// don't hold data in the memory
+// introduce concurrency
+//2 exception handling
+//3 logging
+//4 implement encryption
+//5 implement compression
